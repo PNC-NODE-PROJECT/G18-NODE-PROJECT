5a7b4344fe8c3c254b735f60ef0ecac9cb824bdb
SREYKEA.REUN <SREYKEA.REUN@PNC.LAN>
2022-04-28 15:17:47 +0700

    #3

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,74 @@
-let url = 'http://localhost:5000'
+let url = 'http://localhost:3000';
 
-function displayQuestion(e){
-    e.target.parentElement.parentElement.remove();
-    startQiuz()
+
+function display(){
+    hide(container);
+    hide(displayQuestion);
+    show(createQuestion);
+    hide(container3)
+    
+}
+
+function showBack(){
+    hide(createQuestion);
+    hide(displayQuestion);
+    show(container);
+    hide(container3)
+    
 }
 
 function startQiuz(){
-    let div = document.createElement('div');
-    div.className = 'startApp';
+    show(displayQuestion);
+    hide(createQuestion);
+    hide(container);
+    hide(container3);
 
+}
+function hide(element){
+    element.style.display ="none";
+}
+
+function submitQuiz(){
+    hide(displayQuestion)
+    show(container3)
+    show(subQuiz);
+}
+function show(element){
+    element.style.display ='block';
+}
+
+function addQuestion(){
+    let add = [
+        {
+            question: "Who invented JavaScript?",
+            answers: {
+              a: "Douglas Crockford",
+              b: "Sheryl Sandberg",
+              c: "Brendan Eich"
+            },
+            correctAnswer: "c"
+          },
+    ]
+    console.log(add)
 }
 
 
-
-
+let displayQuestion = document.querySelector('.contain1');
+let createQuestion = document.querySelector('.contain2');
+let container = document.querySelector("#container");
+let container3 = document.querySelector(".contain3");
+let subQuiz = document.querySelector("#submitQuiz");
 
 
 let btn_start = document.querySelector("#start");
-btn_start.addEventListener("click",displayQuestion);
+btn_start.addEventListener("click",startQiuz);
+let btn_create = document.querySelector("#create");
+btn_create.addEventListener("click",display);
+let backTostartQiuz = document.querySelector('#btn-back');
+backTostartQiuz.addEventListener('click',showBack);
+let submit = document.querySelector('#btn-submit');
+submit.addEventListener('click',submitQuiz);
+let back = document.querySelector('#back');
+back.addEventListener('click',showBack);
+let add = document.querySelector('#btn-add');
+add.addEventListener('click',addQuestion);
