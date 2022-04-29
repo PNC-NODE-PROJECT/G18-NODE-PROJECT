f9c218b508ee8edfe6ede12ec21045f22ff9a10b
SREYKEA.REUN <SREYKEA.REUN@PNC.LAN>
2022-04-29 19:07:50 +0700

    #6

--- public/views/playQuiz/play.js
+++ public/views/playQuiz/play.js
@@ -4,6 +4,12 @@ function dataServer() {
         refreshDom(dataServer)
     })
 }
+function getAllQuizzes() {
+    axios.get("http://localhost/api/items/").then((result) => {
+        let dataServer = result.data;
+        refreshDom(dataServer)
+    })
+}
 
 
 function refreshDom(dataServer) {
@@ -28,7 +34,7 @@ function refreshDom(dataServer) {
         input_1.classList = 'form-control mt-1';
         let answer1 = document.createElement("input");
         answer1.setAttribute("type","radio");
-        answer1.value = dataServer[index].answers.A;
+        answer1.value = "A";
         answer1.name = "question"+question;
         answer1.id = "answer_1";
         let label_1 = document.createElement("label");
@@ -40,7 +46,7 @@ function refreshDom(dataServer) {
         input_2.classList = 'form-control mt-1';
         let answer2 = document.createElement("input");
         answer2.setAttribute("type","radio");
-        answer2.value = dataServer[index].answers.A;
+        answer2.value = "B";
         answer2.name = "question"+question;
         answer2.id = "answer_2";
         let label_2 = document.createElement("label");
@@ -52,7 +58,7 @@ function refreshDom(dataServer) {
         input_3.classList = 'form-control mt-1';
         let answer3 = document.createElement("input");
         answer3.setAttribute("type","radio");
-        answer3.value = dataServer[index].answers.A;
+        answer3.value = "C";
         answer3.name = "question"+question;
         answer3.id = "answer_3";
         let label_3 = document.createElement("label");
@@ -64,7 +70,7 @@ function refreshDom(dataServer) {
         input_4.classList = 'form-control mt-1';
         let answer4 = document.createElement("input");
         answer4.setAttribute("type","radio");
-        answer4.value = dataServer[index].answers.A;
+        answer4.value = "D";
         answer4.name = "question"+question;
         answer4.id = "answer_4";
         let label_4 = document.createElement("label");
@@ -86,30 +92,46 @@ function refreshDom(dataServer) {
     }
 }
 //function submit----------------------------------------------------
-function submitQuestion(){
-    let answer = document.querySelectorAll('.')
-}
-//function check score-------------------------------------------------
-function checkAnswer(){
-    // let countScore = 0;
-    let radios = document.querySelectorAll('input[type=radio]');
-        console.log(radios);
-        radios.forEach(radio=>{
-                if(radio.checked){
-                    radio.nextElementSibling.textContent; 
-                    let answersRetrun = false;
-                    if(radio.nextElementSibling.textContent === correct){
-                        answersRetrun = true;
-                        console.log(answersRetrun);
-                    }
 
-                }
-        
-        })
+function submitQuestion(){
+    axios.get("http://localhost/api/items/").then((result) => {
+        let dataServer = result.data;
+        let answers = document.querySelectorAll('input[type=radio]');
+        for (let answer of answers){
+            if(answer.checked){
+                arrayAnswer.push(answer.value);
+            }
+        }
+        for (let index=0;index<arrayAnswer.length ;index++){
+            let answer =dataServer[index].correct;
+            if (answer === arrayAnswer[index]){
+                score ++;
+            }
+        }
+    showResult();
+    })
 }
+function showResult(){
+    document.querySelector('.score').textContent = score;
+    document.querySelector(".result").style.display = "block";
+    displayQuestion.style.display = "none"
+}
+
+// function backTOplay(){
+//     show(getAllQuizzes);
+// }
+
+let currentQuiz = 0
+let score = 0
+let arrayAnswer = []
+
+let back_score = document.querySelector("#btn-score");
+// back_score.addEventListener("click",backTOplay);
+
 let card_question = document.querySelector(".card-question");
 let submit = document.querySelector('#btn-submit');
-submit.addEventListener('click',checkAnswer);
+submit.addEventListener('click',submitQuestion);
+    
 let displayQuestion = document.querySelector('.container-fluid');
 let content_question = document.querySelector('.card');
 dataServer();
